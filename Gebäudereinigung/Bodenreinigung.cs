c9b04ee4d686276f9ce470ccf0f2115c656c5a9b
Jacko04 <Kosterschristian@gmail.com>
2022-03-18 11:51:56 +0100

    ...

--- Gebäudereinigung/Bodenreinigung.cs
+++ Gebäudereinigung/Bodenreinigung.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Gebäudereinigung
 {
+
+
     class Bodenreinigung : Reinigung
     {
+        double kosten;
+        string kunden;
+        string zeit;
+
+        Bodenreinigung(double Kosten, string Kunden, string Zeit, string Datum, string Mitarbeiter, string Putzmaterial);
+       
         int Anzahl_Quadratmeter;
       
     }
